ea58c2e487280524d91cbbeeaaac03060104a655
Tim Ninan <tninan@C02TH0ZXGTFM.local>
2018-10-13 21:46:46 -0500

    Connect to mLab database
    Created mongoDB instance and connected to it.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require("express-graphql");
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//connect to mLab database
+mongoose.connect("mongodb://test-user:test123@ds131763.mlab.com:31763/books-app-db");
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
