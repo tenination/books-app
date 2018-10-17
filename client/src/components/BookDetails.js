f322e8137f9773c526cf3c4c7706ad661fd9a087
Tim Ninan <tninan@C02TH0ZXGTFM.local>
2018-10-17 08:45:28 -0500

    Bind query variable to BookDetails
    Query variable is now bound to getBookQuery. Also, book details
    now displaying for selected book.

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -3,14 +3,41 @@ import { graphql, compose } from 'react-apollo';
 import { getBookQuery } from '../queries/queries';
 
 class BookDetails extends Component {
+  displayBookDetails(){
+    const { book } = this.props.data;
+    if (book) {
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author</p>
+          <ul classNname="other-books">
+            {book.author.books.map((item) =>
+              <li key={item.id}>{item.name}</li>
+            )}
+          </ul>
+        </div>
+      )
+    } else {
+      return (<div>No book selected...</div>)
+    }
+  }
   render() {
     return (
       <div id="book-details">
-        <p>Output book details here </p>
-        {console.log(this.props.bookId)}
+      {this.displayBookDetails()}
      </div>
     );
   }
 }
 
-export default graphql(getBookQuery)(BookDetails);
+export default graphql(getBookQuery, {
+  options: (props) => {
+    return {
+      variables: {
+        id: props.bookId
+      }
+    }
+  }
+})(BookDetails);
