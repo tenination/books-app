b88b2159105777150f1eeb3addc04d5a5329f32c
Tim Ninan <tninan@C02TH0ZXGTFM.local>
2018-10-17 07:52:08 -0500

    Create BookDetails
    Also, create new GraphQL query to fetch information about one book.

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -25,4 +25,22 @@ const addBookMutation = gql`
     }
   }
 `
-export { getAuthorsQuery, getBooksQuery, addBookMutation };
+
+const getBookQuery = gql`
+  query($id: ID) {
+    book(id: $id){
+      id
+      name
+      genre
+      author {
+        name
+        age
+        books {
+          name
+          id
+        }
+      }
+    }
+  }
+`
+export { getAuthorsQuery, getBooksQuery, addBookMutation, getBookQuery };
